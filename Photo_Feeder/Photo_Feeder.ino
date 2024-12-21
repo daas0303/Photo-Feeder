dc823a529d155a99f2015878758b2443203e13fe
Saad <saad.ayub0303@gmail.com>
2024-12-21 13:50:44 -0500

    Structure of code that includes sensor-driven behavior. Not tested in-situ but code compiles with no issues.

--- Photo_Feeder/Photo_Feeder.ino
+++ Photo_Feeder/Photo_Feeder.ino
@@ -1,4 +1,7 @@
 const byte motorSignal_PIN = D2;
+const byte sensor_PIN = A2;
+const int pictureRead_DELAY = 5000; // (ms) delay until checking the sensor again, allows for the picture to get into the scanner before the motor cuts
+const int sensor_THRESHOLD = 512; // Max of 1024, min of 0
 
 void setup() {
   // put your setup code here, to run once:
@@ -6,6 +9,7 @@ void setup() {
   // pinMode(A0, INPUT); // Light sensor
   
   pinMode(motorSignal_PIN, OUTPUT);
+  pinMode(sensor_PIN, INPUT);
 
   Serial.begin(9600);
 
@@ -13,11 +17,19 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
+
+  if (analogRead(sensor_PIN) > sensor_THRESHOLD) {
+    // If it is bright (no picture in slot)
+    delay(2000);
+    digitalWrite(motorSignal_PIN, HIGH);
+  }
   
-  delay(10000);
-  digitalWrite(motorSignal_PIN, HIGH);
-  delay(3000);
-  digitalWrite(motorSignal_PIN, LOW);
+  else {
+    // If there is a picture in da way
+    delay(pictureRead_DELAY);
+    digitalWrite(motorSignal_PIN, LOW);
+  }
+
   Serial.println("Working >B)");
 
 }
